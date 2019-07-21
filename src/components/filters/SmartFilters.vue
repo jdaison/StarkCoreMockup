2d496f5844fc6428d9ba54b2ce2739ddf9513716
John Agudelo <john.agudelo@osgroup.co>
2019-07-21 15:37:57 -0500

    fixes to layout

--- src/components/filters/SmartFilters.vue
+++ src/components/filters/SmartFilters.vue
@@ -3,31 +3,34 @@
     <div class="vertical-align">
       <img id="img-logo" alt="starkcore logo" src="../../assets/logo.png"/>
     </div>
-    <div class="vertical-align border-margin-padding-bottom" >
-      <i class="el-icon-place"></i>
-      <p>Smart Filters</p>
-    </div>
-    <div class="vertical-align">
-      <img id="img-logo" alt="starkcore logo" src="../../assets/sale.png"/>
-      <MySwitch ></MySwitch>
-    </div>
-    <div class="vertical-align">
-      <img id="img-logo" alt="starkcore logo" src="../../assets/calendar.png"/>
-      <MySwitch ></MySwitch>
-    </div>
-    <div class="vertical-align">
-      <img id="img-logo" alt="starkcore logo" src="../../assets/heart.png"/> 
-      <MySwitch ></MySwitch>
-    </div>
+    <div class="rigth-border">
 
-    <div class="vertical-align border-margin-padding-top">
-      <p>Status</p>
-    </div>
-    <div>
-      <input class="searchInput" type="text" placeholder="Search"/>
-    </div>
-    <div class="label">
-      <p  >DELIVERED</p>
+      <div class="vertical-align border-margin-padding-bottom" >
+        <i class="el-icon-place"></i>
+        <p>Smart Filters</p>
+      </div>
+      <div class="vertical-align">
+        <img id="img-logo" alt="starkcore logo" src="../../assets/sale.png"/>
+        <MySwitch ></MySwitch>
+      </div>
+      <div class="vertical-align">
+        <img id="img-logo" alt="starkcore logo" src="../../assets/calendar.png"/>
+        <MySwitch ></MySwitch>
+      </div>
+      <div class="vertical-align">
+        <img id="img-logo" alt="starkcore logo" src="../../assets/heart.png"/> 
+        <MySwitch ></MySwitch>
+      </div>
+
+      <div class="vertical-align border-margin-padding-top">
+        <p>Status</p>
+      </div>
+      <div>
+        <input class="searchInput" type="text" placeholder="Search"/>
+      </div>
+      <div class="label">
+        <p  >DELIVERED</p>
+      </div>
     </div>
  
   </ElContainer>
